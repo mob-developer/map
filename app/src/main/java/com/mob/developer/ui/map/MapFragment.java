225c7c4c19bd20fbc992704ca45339fc3ed04912
mirkamalice <mirkamalice@gmail.com>
2021-04-27 00:22:49 +0430

    show map

--- app/src/main/java/com/mob/developer/ui/map/MapFragment.java
+++ app/src/main/java/com/mob/developer/ui/map/MapFragment.java
@@ -12,22 +12,54 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 import com.mob.developer.R;
 
 public class MapFragment extends Fragment {
 
     private MapViewModel mapViewModel;
+    private MapView mapView;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        mapViewModel =
-                new ViewModelProvider(this).get(MapViewModel.class);
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mapViewModel = new ViewModelProvider(this).get(MapViewModel.class);
+        Mapbox.getInstance(getContext(), getString(R.string.mapbox_access_token));
         View root = inflater.inflate(R.layout.fragment_map, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+//        final TextView textView = root.findViewById(R.id.text_dashboard);
+        mapView = (MapView) root.findViewById(R.id.mapView);
         mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+//                textView.setText(s);
+                mapView.onCreate(savedInstanceState);
+                mapView.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+
+                        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                            @Override
+                            public void onStyleLoaded(@NonNull Style style) {
+
+                                // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+
+                                CameraPosition position = new CameraPosition.Builder().target(new LatLng(32.4207, 53.68)).zoom(4).build();
+
+                                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 2000);
+
+
+
+                            }
+                        });
+
+                    }
+                });
             }
         });
         return root;
